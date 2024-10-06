acb81ee9a07a7380b6bbd81a24d6043a143f5fd8
Vincent <paulvincentcalvo.pr@gmail.com>
2024-10-06 23:34:46 +0800

    added projects section

--- lib/main.dart
+++ lib/main.dart
@@ -332,10 +332,98 @@ class ProjectsPage extends StatelessWidget {
           fontSize: 20,
         ),
       ),
-      body: const Center(
-        child: Text(
-          'This is the Projects section.',
-          style: TextStyle(fontSize: 24),
+      body: Container(
+        padding: const EdgeInsets.all(16.0), 
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'CipherLock',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Description: A Cipher program intended to encrypt or decrypt messages using various ciphers.',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Link: https://github.com/PaulVincent-Calvo/CipherLock',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Role: Project Lead',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Treasury Citadel',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Description: A Banking System simulation using Tkinter Designer (Python) and MySQL.',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Link: https://github.com/PaulVincent-Calvo/Treasury-Citadel',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Role: Back-end Developer',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Sweet Liberty',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Description: A text-based, windows form game that follows the story of Alpha-Three, a stranded Helldiver on a planet. You have to explore areas and use your critical thinking to use and hold items in order to survive through multiple encounters.',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Link: https://github.com/Kryptiku/Sweet-Liberty',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              'Role: Front-end Developer',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
         ),
       ),
     );
